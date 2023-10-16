16d60a77fbae5419f33b34678e12cf560f49f7c6
kusabakosuke <kousukekusaba@gmail.com>
2023-10-16 22:02:46 +0900

    3の倍数を学習しました

--- 01_テキスト/00_JavaScriptの関数/main.js
+++ 01_テキスト/00_JavaScriptの関数/main.js
@@ -26,5 +26,19 @@ const f3 = function (n) {
 
 // TRY: 練習問題３
 // ① ２つの引数を受け取り、それらをかけ合わせた値を返す関数 multiply を作成して正しく動くかテストしてください。
-
+const multiply = function (x, y) {
+  return x * y
+}
 // ② ２つの引数を受け取り、それらを足し合わせた結果を四捨五入した値を返す関数 plusRound を作成して正しく動くかテストしてください。
+const plusRound = function (x, y) {
+  return Math.round(x + y)
+}
+const sayHello = function (name) {
+  alert("Hello" + name + "!")
+}
+sayHello("guys")
+
+const alertTime = function () {
+  alert(new Date().toLocaleString())
+}
+alertTime()
