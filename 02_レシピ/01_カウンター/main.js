f932fbbee9f498253c0ce3c863ceed82887520a0
kusabakosuke <kousukekusaba@gmail.com>
2023-10-17 16:33:56 +0900

    カウンターを作成しました

--- 02_レシピ/01_カウンター/main.js
+++ 02_レシピ/01_カウンター/main.js
@@ -1,8 +1,20 @@
 const display = document.getElementById("display")
 const plusbutton = document.getElementById("plus-button")
+const minusbutton = document.getElementById("minus-button")
+const nibaibutton = document.getElementById("nibai-button")
 
 let count = 0
 plusbutton.onclick = function () {
   count += 1
+  display.textContent = count
+}
+
+minusbutton.onclick = function () {
+  count -= 1
+  display.textContent = count
+}
+
+nibaibutton.onclick = function () {
+  count = count * 2
   display.textContent = count
 }
